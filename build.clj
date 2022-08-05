eee718168de60af46f4918f7916196e34453b428
Kenny Williams <kwill@hey.com>
2022-08-05 14:31:01 -0700

    Add build

---
+++ build.clj
@@ -0,0 +1,18 @@
+(ns build
+  (:require
+    [clojure.tools.build.api :as b]
+    [org.corfield.build :as bb]))
+
+(def lib 'dev.kwill/table)
+(def version (format "0.1.%s" (b/git-count-revs nil)))
+
+(defn jar "Build lib jar." [opts]
+  (-> (assoc opts :lib lib :version version)
+    (bb/clean)
+    (bb/jar))
+  opts)
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (-> opts
+    (assoc :lib lib :version version)
+    (bb/deploy)))
